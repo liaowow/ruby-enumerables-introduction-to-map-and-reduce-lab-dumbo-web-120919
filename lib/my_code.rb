fded0989aee3576e3c774eb01b51a9e2d79cf5aa
Annie Liao <liaowow@yahoo.com>
2019-11-17 21:27:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map_to_negativize(arr)
   new_arr = []
   i = 0
   while i < arr.length do
-    new_arr << (arr[i] * -1)
+    new_arr << arr[i] * -1
     i += 1
   end
   new_arr
@@ -21,8 +21,21 @@ def map_to_no_change(arr)
 end
 
 def map_to_double(arr)
-  
+  new_arr = []
+  i = 0
+  while i < arr.length do
+    new_arr << arr[i] * 2
+    i += 1
+  end
+  new_arr
 end
 
 def map_to_square(arr)
+  new_arr = []
+  i = 0
+  while i < arr.length do
+    new_arr << arr[i] ** 2
+    i += 1
+  end
+  new_arr
 end
