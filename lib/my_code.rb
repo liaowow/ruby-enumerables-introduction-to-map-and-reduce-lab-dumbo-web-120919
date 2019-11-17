627fa250526166c9bcfc3c225beb613cd4517257
Annie Liao <liaowow@yahoo.com>
2019-11-17 21:19:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,20 @@
 # My Code here....
+# map-like
+def map_to_negativize(source_array)
+  new_arr = []
+  i = 0
+  while i < source_array.length do
+    source_array[i] * -1
+    new_arr << source_array[i]
+  end
+  return new_arr
+end
+
+def map_to_no_change(arr)
+end
+
+def map_to_double(arr)
+end
+
+def map_to_square(arr)
+end
